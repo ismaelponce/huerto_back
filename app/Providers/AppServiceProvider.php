e671791b2719cf2c143467e69588fa67b708219c
Ismael Ponce <ismaelponceon@gmail.com>
2022-10-27 17:45:44 +0200

    Cambio prueba

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\facades\Schema;
-
+use Illuminate\Support\Facades\schema;
 
 class AppServiceProvider extends ServiceProvider
 {
